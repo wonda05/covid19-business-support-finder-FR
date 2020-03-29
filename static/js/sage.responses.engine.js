ae4601cdab74021ac5575527a3cc54ac2dbf3c37
Jamie Weston <monotone2k@gmail.com>
2020-03-29 14:13:18 +0100

    Displaying the user's answers back to them when no guidance found

--- static/js/sage.responses.engine.js
+++ static/js/sage.responses.engine.js
@@ -3,7 +3,12 @@
 (function ($) {
     var guidence_template = '<div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left guidence" ><hr><h3>TITLE</h3></div></div><div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left"><h6>TEXT</h6></div></div>'
     var link_template = '<div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left link"><a href="URL" onclick="trackOutboundLink(TITLE); return true;" target="_blank">TEXT<img class="url_arrow" src="static/images/url_arrow.svg"></a></div></div>';
+    var questions_heading = '<div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left"><h3>Your responses were</h3></div></div>';
+    var question_template = '<div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left"><h6>QUESTION</h6></div></div>';
+    var answer_template = '<div class="row justify-content-center"><div class="col-lg-8 col-md-10 text-left"><p>ANSWER</p></div></div>';
+
     var responses;
+    var questions;
 
     var tryLoadResponses = function(callback) {
         var test = (window.sfcoronavirus) && (window.sfcoronavirus.responses);
@@ -12,6 +17,7 @@
             if (!test) {
                 tryLoadResponses(callback);
             } else {
+                questions = window.sfcoronavirus.questions;
                 responses = window.sfcoronavirus.responses;
                 callback();
             }
@@ -55,23 +61,65 @@
         trySendAnalytics('CoronavirusFunding', 'RestartFlow', 'N/A', callback);
     };
 
+    var showUserAnswers = function(answers) {
+        // start with only the header, we'll add to this as we match the user's responses back to Q&A text labels
+        var userAnswers = questions_heading;
+
+        for (var i = 0; i < questions.length; i++) {
+            var q = questions[i];
+
+            // check if question has any answers (some questions are optional depending on flow)
+            var answersToQ = answers.filter(function(answer) {
+                return answer.charAt(0) === q.number + '';
+            });
+
+            if (answersToQ.length) {
+                // if the user answered a question, we must display the question text
+                userAnswers = userAnswers.concat(question_template.replace('QUESTION',q.question.title));
+
+                var arrayOfAnswerLabels = [];
+
+                // then we add the answer text to the view (comma-separated in the case of multiple choice)
+                for (var j = 0; j < answersToQ.length; j++) {
+                    var a = q.answers.find(function(answer) {
+                        return answer.value === answersToQ[j].charAt(1);
+                    });
+                    arrayOfAnswerLabels.push(a.text);
+                }
+                var stringOfAnswerLabels = arrayOfAnswerLabels.join(', ');
+                userAnswers = userAnswers.concat(answer_template.replace('ANSWER',stringOfAnswerLabels));
+            }
+        }
+
+        document.getElementById('guidance-items').innerHTML = userAnswers;
+    };
+
     var fillPage = function () {
         var answers = JSON.parse(localStorage.getItem('answers'));
-        var guidences = '';
+        var guidanceItems = '';
+
+        var guidanceFound = false;
 
         for (var i = 0; i < responses.length; i++) {
             var r = responses[i];
             if(checkPrereqs(r.prerequisites, answers)) {
-                guidences = guidences.concat(guidence_template.replace('TITLE',r.guidance.title).replace('TEXT', r.guidance.text));
+                guidanceFound = true;
+                guidanceItems = guidanceItems.concat(guidence_template.replace('TITLE',r.guidance.title).replace('TEXT', r.guidance.text));
                 for(var j=0;j<r.links.length;j++) {
                     var link = r.links[j];
-                    guidences = guidences.concat(link_template.replace('TEXT', link.text).replace('URL', link.url).replace('TITLE', "'" + r.guidance.title + "'"));
+                    guidanceItems = guidanceItems.concat(link_template.replace('TEXT', link.text).replace('URL', link.url).replace('TITLE', "'" + r.guidance.title + "'"));
                 }
             }
         }
 
-        document.getElementById('guidences').innerHTML = guidences;
-    }
+        if (guidanceFound) {
+            $('#guidance-summary').show();
+            document.getElementById('guidance-items').innerHTML = guidanceItems;
+        } else {
+            $('#no-guidance-found').show();
+            showUserAnswers(answers);
+        }
+    };
 
     // needs to be global as it's called directly by onclick handler
     window.trackOutboundLink = function(topic) {
@@ -88,8 +136,13 @@
         var restartButton = $('#restart_button')[0];
         if (restartButton !== null) {
             restartButton.onclick = restart;
-            init();
         }
+
+        $('#restart_link').on('click', function(event) {
+            event.preventDefault();
+            restart();
+        });
+
         init();
     });
 })(jQuery);
